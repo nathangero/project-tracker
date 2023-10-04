14858be578c64978c3d32e0e1520c08a4155c02d
nathangero <nathangero@yahoo.com>
2023-10-04 12:05:11 -0700

    start coding saving project info

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ var today = dayjs();
 $(function() {
     $("#currentDay").text(today.format("MMM DD, YYYY [at] hh:mm:ss a"));
 
+    $("#btn-add-project").on("click", saveProject);
+
     setInterval(function() {
         today = dayjs();
         $("#currentDay").text(today.format("MMM DD, YYYY [at] hh:mm:ss a"));
     }, 1000)
 })
+
+function saveProject(event) {
+    
+}
